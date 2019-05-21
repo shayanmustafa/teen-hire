3e93e06c24b61ff6af9cf3530fcaf12d2b3d89d3
Kumail Pirzada <kumailpirzada@gmail.com>
2019-05-21 05:08:41 +0500

    Employer list completed

--- TeenHire/src/components/employer.js
+++ TeenHire/src/components/employer.js
@@ -1,63 +1,86 @@
-import React, { Component } from 'react'
-import { StyleSheet, Text, View, TextInput, Button, ToastAndroid, FlatList, StatusBar } from 'react-native'
-import { Container, Content, Card, CardItem, Body } from 'native-base';
-import firebase from '../common/firebase1'
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ToastAndroid,
+  FlatList,
+  StatusBar
+} from "react-native";
+import { Container, Content, Card, CardItem, Body } from "native-base";
+import firebase from "../common/firebase1";
 
 export default class Employer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            jobsPosted: '',
-            applyCount: 0
-        }
-    }
-    componentDidMount() {
-        var user = firebase.auth().currentUser.uid;
-        firebase.firestore().collection("posts").where("employerID", "==", user).get().then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                this.setState({
-                    jobsPosted: [...this.state.jobsPosted, doc.data()]
-                });
-            });
-        })
-    }
-    render() {
-        return (
-            <View>
-                <FlatList
-                data={this.state.jobsPosted}
-                renderItem={({item}) =>
-                    <Container  style={styles.cardContainer}>
-                        <Content>
-                            <Card>
-                                <CardItem header>
-                                        <Text style={{color: "black", fontWeight: "bold", textAlign: "center"}}>{item.jobTitle}</Text>
-                                </CardItem>
-                                <CardItem>
-                                    <Body>
-                                        <Text style={{color: "black"}}>{item.jobDescription}</Text>
-                                    </Body>
-                                </CardItem>
-                            </Card>
-                        </Content>
-                    </Container>
-                }/>
-            </View>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      jobsPosted: "",
+      applyCount: 0
+    };
+  }
+  componentDidMount() {
+    var user = firebase.auth().currentUser.uid;
+    firebase
+      .firestore()
+      .collection("posts")
+      .where("employerID", "==", user)
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          this.setState({
+            jobsPosted: [...this.state.jobsPosted, doc.data()]
+          });
+        });
+      });
+  }
+  render() {
+    return (
+      <FlatList
+        data={this.state.jobsPosted}
+        renderItem={({ item }) => (
+          <Card>
+            <CardItem
+              header
+              style={{
+                backgroundColor: "#3cc194"
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 20,
+                  fontWeight: "800",
+                  color: "#116d4e",
+                  fontWeight: "bold",
+                  textAlign: "center"
+                }}
+              >
+                {item.jobTitle}
+              </Text>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text style={{ color: "black" }}>{item.jobDescription}</Text>
+              </Body>
+            </CardItem>
+          </Card>
+        )}
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    cardContainer: {
-        flexDirection: 'column',
-        backgroundColor: "#06152a"
-    },
-    titleContainer: {
-        marginBottom: 2
-    },
-    buttonContainer: {
-        justifyContent: 'center',
-        flexDirection: 'row',
-        marginBottom: 30,
-    }
-})
+  cardContainer: {
+    backgroundColor: "#06152a"
+  },
+  titleContainer: {
+    marginBottom: 2
+  },
+  buttonContainer: {
+    justifyContent: "center",
+    flexDirection: "row",
+    marginBottom: 30
+  }
+});
